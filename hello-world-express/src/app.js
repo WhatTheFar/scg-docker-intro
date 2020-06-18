8003377fa277845dfc2dabcd2bb850301b4361cb
WhatTheFar <jakpat.m@gmail.com>
2020-06-18 10:14:54 +0700

    feat: v5

--- hello-world-express/src/app.js
+++ hello-world-express/src/app.js
@@ -6,5 +6,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/v2', (req, res) => res.send('Hello World!!'))
 app.get('/v3', (req, res) => res.send('Hello World!!!'))
 app.get('/v4', (req, res) => res.send('Hello World!!!!'))
+app.get('/v5', (req, res) => res.send('Hello World!!!!!'))
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
